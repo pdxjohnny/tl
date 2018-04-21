579381b83d0f8480b4463da3f967b215560e980c
John Andersen <johnandersenpdx@gmail.com>
2018-04-21 11:22:48 -0700

    tl: Initial commit
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

---
+++ js/core.js
@@ -0,0 +1,406 @@
+class Resource {
+  constructor(sync, name, typename, meta, value) {
+    // Sync
+    if (typeof sync !== 'object') {
+      throw new Error('sync must be an array of Syncs');
+    }
+    this.sync = sync;
+    // Name
+    if (typeof name !== 'string') {
+      throw new Error('name must be a string');
+    }
+    this.name = name;
+    // Meta
+    if (typeof meta !== 'object') {
+      meta = {};
+    }
+    if (typeof typename !== 'string') {
+      throw new Error('typename must be a string');
+    }
+    this.meta = meta;
+    this.meta['__type'] = typename;
+    // Value
+    this.values = [];
+    this.value = undefined;
+    if (typeof value !== 'undefined') {
+      this.value = value;
+    }
+    this.callbacks = [];
+    this.bound = new Bound();
+  }
+  primarysyncget() {
+    return this._syncget(this.sync.slice(0, 1));
+  }
+  syncget() {
+    return this._syncget(this.sync);
+  }
+  _syncget(syncs) {
+    return Promise.all(syncs.map(function(sync) {
+      return sync.get(this)
+      .then((function(sync) {
+        return function(value) {
+          if (value !== null && value !== undefined) {
+            this.onupdate(value);
+          }
+          this.runcallbacks(value);
+          return value;
+        }.bind(this);
+      }.bind(this))(sync));
+    }.bind(this)));
+  }
+  onempty(callback) {
+    if (typeof callback !== 'function') {
+      throw new Error('callback must be function');
+    }
+    this.callbacks.push({
+      callback: callback,
+      empty: true,
+      once: false
+    });
+  }
+  register(callback) {
+    if (typeof callback !== 'function') {
+      throw new Error('callback must be function');
+    }
+    this.callbacks.push({
+      callback: callback,
+      once: false
+    });
+  }
+  oneshot(callback) {
+    if (typeof callback !== 'function') {
+      throw new Error('callback must be function');
+    }
+    this.callbacks.push({
+      callback: callback,
+      once: true
+    });
+  }
+  unregister(callback) {
+    if (typeof callback !== 'function') {
+      throw new Error('callback must be function');
+    }
+    var remove = -1;
+    for (var i = 0; i < this.callbacks.length; ++i) {
+      if (this.callbacks[i].callback === callback) {
+        remove = i;
+      }
+    }
+    if (remove !== -1) {
+      this.callbacks.splice(remove, 1);
+    }
+  }
+  queryPrimary() {
+    return this.primarysyncget()
+    .then(function(values) {
+      if (values.length) {
+        return values[0];
+      }
+      return null;
+    }.bind(this));
+  }
+  query() {
+    return this.syncget();
+  }
+  update(value) {
+    if (typeof value === 'undefined') {
+      throw new Error('update requires value');
+    }
+    if (typeof this.meta['__type'] === undefined) {
+      throw new Error('update requires meta to contain __type');
+    }
+    return this._update(this.sync, value);
+  }
+  _update(syncs, value) {
+    return Promise.race(syncs.map(function(sync) {
+      return sync.set(this, value);
+    }.bind(this)))
+    .then(function() {
+      this.value = value;
+      this.runcallbacks(value);
+      return this;
+    }.bind(this));
+  }
+  onupdate(value) {
+    return this.validateupdate(value)
+    .then(function(value) {
+      this.value = value;
+      this.runcallbacks(value);
+      return value;
+    }.bind(this));
+  }
+  validateupdate(value) {
+    return Promise.resolve(value);
+  }
+  runcallbacks(value) {
+    var remove = [];
+    for (var i in this.callbacks) {
+      if ((typeof value !== 'undefined' && value !== null) ||
+          (typeof this.callbacks[i].empty !== 'undefined' &&
+          this.callbacks[i].empty === true)) {
+        this.callbacks[i].callback(value, this);
+      }
+      if (typeof this.callbacks[i].once !== 'undefined' &&
+          this.callbacks[i].once) {
+        remove.push(i);
+      }
+    }
+    for (var i = 0; i < remove.length; ++i) {
+      this.callbacks.splice(remove[i], 1);
+    }
+  }
+  pack(value) {
+    return Promise.resolve({
+      name: this.name,
+      meta: this.meta,
+      value: value
+    });
+  }
+  unpack(packed) {
+    if (packed === null) {
+      return null;
+    }
+    if (typeof packed !== 'object') {
+      throw new Error('packed must be object');
+    }
+    this.meta = packed.meta;
+    return Promise.resolve(packed.value);
+  }
+  marshal(value) {
+    return Promise.resolve(value);
+  }
+  unmarshal(value) {
+    return Promise.resolve(value);
+  }
+  prestore(value) {
+    return Promise.resolve(value);
+  }
+  poststore(value) {
+    return Promise.resolve(value);
+  }
+}
+
+class Sync extends Resource {
+  // pre and post can be used to encrypt and decrypt data
+  constructor(sync, name, pre, post, relay, meta, value) {
+    super(sync, name, 'sync', meta, value);
+    if (!Array.isArray(pre)) {
+      throw new Error('pre must be an array of processors');
+    }
+    if (!Array.isArray(post)) {
+      throw new Error('post must be an array of processors');
+    }
+    if (!Array.isArray(relay)) {
+      relay = [];
+    }
+    this.pre = pre;
+    this.post = post;
+    this.relay = relay;
+    this.watching = {};
+  }
+  preprocess(resource, value) {
+    return resource.marshal(value)
+    .then(function(value) {
+      return this.runthrough(this.pre, 'pre', resource, value);
+    }.bind(this))
+    .then(function(preprocessed) {
+      return resource.prestore(preprocessed);
+    }.bind(this))
+    .then(function(prestore) {
+      return resource.pack(prestore);
+    }.bind(this))
+    .then(function(packed) {
+      return this.runthrough(this.pre, 'pre', resource, packed);
+    }.bind(this));
+  }
+  postprocess(resource, value) {
+    return this.runthrough(this.post, 'post', resource, value)
+    .then(function(packed) {
+      return resource.unpack(packed);
+    }.bind(this))
+    .then(function(poststore) {
+      return resource.poststore(poststore);
+    }.bind(this))
+    .then(function(poststore) {
+      return this.runthrough(this.post, 'post', resource, poststore);
+    }.bind(this))
+    .then(function(value) {
+      return resource.unmarshal(value);
+    }.bind(this));
+  }
+  runthrough(array, func, resource, value) {
+    return new Promise(function(resolve, reject) {
+      var i = 0;
+      var curr = value;
+      if (curr === null || curr === undefined) {
+        return resolve(null);
+      }
+      if (array.length < 1) {
+        return resolve(curr);
+      }
+      var inc = function() {
+        try {
+          array[i][func](resource, curr).then(function(processed) {
+            curr = processed;
+            ++i;
+            if (i < array.length) {
+              setTimeout(inc, 0);
+            } else {
+              return resolve(curr);
+            }
+          }.bind(this)).catch(reject);
+        } catch (err) {
+          reject(err);
+        }
+      }.bind(this)
+      setTimeout(inc, 0);
+    }.bind(this));
+  }
+  get(resource) {
+    return Promise.resolve(null);
+  }
+  set(resource, value) {
+    return Promise.resolve(resource);
+  }
+}
+
+class Bound {
+  constructor() {
+    this.bound = [];
+  }
+  method(method, bound_to) {
+    for (var i = 0; i < this.bound; ++i) {
+      if (this.bound[i][0] === method && this.bound[i][1] === bound_to) {
+        return this.bound[i][2];
+      }
+    }
+    var bound = method.bind(bound_to);
+    this.bound.push([method, bound_to, bound]);
+    return bound;
+  }
+}
+
+// https://stackoverflow.com/questions/610406/javascript-equivalent-to-printf-string-format
+if (!String.prototype.format) {
+  String.prototype.format = function() {
+    var args = arguments;
+    const formatValue = function(value) {
+      if (typeof value === 'boolean') {
+        if (value === true) {
+          return 'YUP';
+        } else {
+          return 'NOPE';
+        }
+      }
+      return value;
+    }
+    return this.replace(/{(\d+)}/g, function(match, number) {
+      return typeof args[number] !== 'undefined'
+        ? formatValue(args[number])
+        : match
+      ;
+    });
+  };
+}
+class Dict extends Resource {
+  constructor(sync, name, typename, subClass, meta, value) {
+    super(sync, name, typename, meta, value);
+    this.subClass = subClass;
+    this.subvalue = {};
+    if (typeof this.value !== 'array') {
+      this.value = [];
+    }
+  }
+  loadSubvalue() {
+    var subvaluesLoading = [];
+    for (var key in this.value) {
+      subvaluesLoading.push(this._load_subvalue(this.value[key]));
+    }
+    return Promise.all(subvaluesLoading).then(function() {
+      return this.subvalue;
+    }.bind(this));
+  }
+  update(value, resource) {
+    value = Array.from(new Set(value));
+    if (typeof resource !== 'undefined') {
+      this.subvalue[resource.name] = resource;
+    }
+    return super.update(value).then(function(resource) {
+      return this.loadSubvalue().then(function() {
+        return this;
+      }.bind(this));
+    }.bind(this));
+  }
+  load() {
+    return super.load().then(function() {
+      return this.loadSubvalue().then(function() {
+        console.log('Subvalues Loaded', this.name, this.value, this.subvalue);
+        return this;
+      }.bind(this));
+    }.bind(this));
+  }
+  contains(key) {
+    return Promise.resolve(this.value.includes(key));
+  }
+  add(key, resource) {
+    if (key.length < 1) {
+      return Promise.resolve(this);
+    }
+    var value = JSON.parse(JSON.stringify(this.value));
+    value.push(key);
+    console.log('Adding Subvalue', this.name, key, resource);
+    return this.update(value, resource);
+  }
+  remove(key) {
+    var index = this.value.indexOf(key);
+    if (key.length < 1 || index == -1) {
+      return Promise.resolve(this);
+    }
+    console.log('Removing Subvalue', this.name, key);
+    var value = JSON.parse(JSON.stringify(this.value));
+    value.splice(index, 1);
+    return this.update(value);
+  }
+  _load_subvalue(key, resource) {
+    if (this.subvalue.hasOwnProperty(key)) {
+      return Promise.resolve(this.subvalue[key]);
+    }
+    if (this.value.includes(key)) {
+      resource = this.createSubvalue(key);
+      return resource.load().then(function(resource) {
+        this.subvalue[resource.name] = resource;
+        return resource;
+      }.bind(this));
+    }
+    console.log(this.value, key);
+    return Promise.reject(new Error('not subvalues'));
+  }
+  createSubvalue(key) {
+    return new this.subClass(this.sync, key);
+  }
+}
+class JSONProcessor {
+  pre(resource, value) {
+    return Promise.resolve(JSON.stringify(value));
+  }
+  post(resource, value) {
+    return Promise.resolve(JSON.parse(value));
+  }
+}
+class LocalStorageSync extends Sync {
+  constructor(sync, name, pre, post, meta, value) {
+    super(sync, name, pre, post, meta, value);
+  }
+  get(resource) {
+    var value = localStorage.getItem(this.name + '.' + resource.name)
+    return this.postprocess(resource, value);
+  }
+  set(resource, value) {
+    return this.preprocess(resource, value)
+    .then(function(preprocessed) {
+      localStorage.setItem(this.name + '.' + resource.name, preprocessed);
+      console.warn('LocalStorageSync.set', resource.name, preprocessed);
+      return resource;
+    }.bind(this));
+  }
+}
---
+++ js/ui.js
@@ -0,0 +1,274 @@
+class Notify {
+  constructor(app, element, timeout) {
+    if (typeof timeout === 'undefined') {
+      timeout = 2000;
+    }
+    this.element = element;
+    this.timeout = timeout;
+    this.queue = [];
+    this.visable = false
+    this.processing = false;
+    this.timer = undefined;
+  }
+  send(msg) {
+    this.process({
+      text: msg,
+      color: 'rgba(107, 227, 232, 0.05)'
+    });
+  }
+  error(msg) {
+    this.process({
+      text: msg,
+      color: 'rgba(231, 109, 105, 0.05)'
+    });
+  }
+  process(msg) {
+    if (msg === undefined) {
+      return;
+    }
+    if (this.timer !== undefined) {
+      clearTimeout(this.timer);
+    }
+    this.element.innerText = msg.text;
+    this.element.style.marginBottom = '0px';
+    this.element.style.display = 'block';
+    this.element.style.textAlign = 'center';
+    this.element.style.backgroundColor = msg.color;
+    this.timer = setTimeout(function() {
+      this.element.style.display = 'none';
+    }.bind(this), this.timeout);
+  }
+  clear() {
+    this.element.style.display = 'none';
+  }
+}
+
+class View {
+  constructor(app, element) {
+    this.app = app;
+    this.element = element;
+    if (typeof this.element !== 'undefined') {
+      this.element.onkeyup = this.onkeyup.bind(this);
+    }
+  }
+  onkeyup() {}
+  reload() {
+    var div = document.createElement('div');
+    this.element.innerHTML = '';
+    this.element.appendChild(div);
+    return div;
+  }
+}
+
+class List extends View {
+  constructor(app, element, resource, listel, empty) {
+    super(app, element);
+    if (typeof empty === 'string') {
+      this.empty = empty;
+    } else {
+      this.empty = 'Nothing to see here...';
+    }
+    this.resource = resource;
+    this.resource.register(this.reload.bind(this));
+    this.resource.onempty(this.reload.bind(this));
+    this.listel = listel;
+  }
+  reload() {
+    return this.list()
+    .then(function(list) {
+      this.element.innerText = '';
+      if (Object.keys(list).length < 1) {
+        this.element.appendChild(this.noItems());
+      } else {
+        this.element.style.textAlign = 'left';
+      }
+      var lastel = this.element;
+      for (var key in list) {
+        var listel = this.listel(this.app, document.createELement('div'),
+            list[key]);
+        listel.element.onload = function() {
+          lastel.element.scrollIntoView();
+        }
+        this.element.appendChild(listel.element);
+      }
+    }.bind(this));
+  }
+  list() {
+    return Promise.resolve(this.resource.subvalue);
+  }
+  noItems() {
+    var noItems = document.createElement('h2');
+    noItems.className = 'mui-panel';
+    noItems.style.textAlign = 'center';
+    noItems.innerText = this.empty;
+    return noItems;
+  }
+}
+
+class Input extends View {
+  constructor(resource, propName, labelText, className, typeName) {
+    super(resource.app, undefined);
+    if (typeof typeName === 'undefined') {
+      typeName = 'text';
+    }
+    this.resource = resource;
+    this.inputId = String(Math.random());
+    this.element = document.createElement('div');
+    this.input = document.createElement('input');
+    this.label = document.createElement('label');
+    this.input.setAttribute('id', this.inputId);
+    this.label.setAttribute('for', this.inputId);
+    this.label.innerText = labelText;
+    this.element.appendChild(this.input);
+    this.element.appendChild(this.label);
+    this.element.className = className;
+    this.input.setAttribute('type', typeName);
+    this.input.onchange = function(event) {
+      var value = JSON.parse(JSON.stringify(this.resource.value));
+      if (typeof typeName !== 'undefined' && typeName === 'checkbox') {
+        value[propName] = event.target.checked;
+      } else if (propName === 'name') {
+        this.resource.name = event.target.value;
+      } else {
+        value[propName] = event.target.value;
+      }
+      this.resource.update(value);
+    }.bind(this);
+    if (typeof typeName !== 'undefined' && typeName === 'checkbox') {
+      this.input.checked = this.resource.value[propName];
+    } else if (propName === 'name') {
+      if (typeof this.resource.name === 'string') {
+        this.input.value = this.resource.name;
+      } else {
+        this.input.value = '';
+      }
+    } else {
+      this.input.value = this.resource.value[propName];
+    }
+  }
+}
+
+class Button {
+  constructor(text, className) {
+    this.element = document.createElement('button');
+    this.element.innerText = text;
+    this.element.className = className;
+  }
+}
+
+class Listel extends View {
+  constructor(app, element, resource, modal) {
+    super(app, element);
+    this.resource = resource;
+    this.modal = modal;
+  }
+  reload() {
+    var div = super.reload();
+    div.dismissed = function() {};
+    div.className = 'mui-panel';
+    var title = document.createElement('h2');
+    div.appendChild(title);
+    title.innerText = this.resource.name;
+    if (typeof this.resource.value === 'object' &&
+        typeof this.resource.label === 'object') {
+      for (var prop in this.resource.value) {
+        var desc = document.createElement('p');
+        div.appendChild(desc);
+        desc.innerText = this.resource.label[prop].format(
+            this.resource.value[prop]);
+      }
+    } else {
+      var desc = document.createElement('p');
+      div.appendChild(desc);
+      desc.innerText = this.resource.value;
+    }
+    div.onclick = function(event) {
+      this.app.popup(this.modal(this.app, resource));
+    }.bind(this);
+  }
+}
+
+class App {
+  constructor(element) {
+    this.element = element;
+    this.jsonp = new JSONProcessor();
+    this.processors = {
+      'pre': [this.jsonp],
+      'post': [this.jsonp]
+    };
+    this.localstorage = new LocalStorageSync([], 'localstorage',
+        this.processors.pre, this.processors.post);
+    this.sync = [this.localstorage];
+  }
+  popup(element, isstatic) {
+    var modal = document.createElement('div');
+    modal.className = 'mui-container-fluid';
+
+    var container = document.createElement('div');
+    container.className = 'mui-panel';
+
+    container.style.position = 'absolute';
+    container.style.top = '80px';
+    container.style.left = '20px';
+    container.style.right = '20px';
+
+    container.appendChild(element);
+    modal.appendChild(container);
+
+    this.notify.clear();
+    if (typeof isstatic === 'undefined') {
+      isstatic = false;
+    }
+    mui.overlay('on', {static: isstatic}, modal);
+  }
+  popdown() {
+    mui.overlay('off');
+  }
+  mainview(view) {
+    for (var child = 0; child < this.element.children.length; ++child) {
+      this.element.children[child].style.display = 'none';
+    }
+    this.element.innerText = '';
+    this.element.appendChild(view.element);
+    console.log('Main View is now', view);
+    for (var child = 0; child < this.element.children.length; ++child) {
+      this.element.children[child].style.display = 'block';
+    }
+  }
+}
+
+class Loading extends View {
+  constructor(app, title, element) {
+    super(app, element);
+    this.title = title;
+    this.element = document.createElement('div');
+    this.loader = this.regen();
+    this.element.appendChild(this.loader);
+  }
+  changeTitle(title) {
+  	this.title = title;
+  	return this.reload();
+  }
+  reload() {
+    this.element.removeChild(this.loader);
+    this.loader = this.regen();
+    this.element.appendChild(this.loader);
+  }
+  regen() {
+    var div = document.createElement('div');
+    for (var className of ['tl-loading-circle', 'tl-loading-circle-small',
+        'tl-loading-circle-big', 'tl-loading-circle-inner-inner',
+        'tl-loading-circle-inner']) {
+      var child = document.createElement('div');
+      child.className = className;
+      div.appendChild(child);
+    }
+    if (typeof this.title !== 'undefined') {
+      var title = document.createElement('center');
+      title.className = 'tl-loading-title';
+      title.innerText = this.title;
+      div.appendChild(title);
+    }
+    return div;
+  }
+}
